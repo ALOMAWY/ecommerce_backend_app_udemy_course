01f8122939c80872522da87c572dcef666900818
Abdalrahman Aldabbas <aldabbas333abdalrahman@gmail.com>
2025-10-18 23:44:23 +0300

    Add Files

---
+++ dist/routes/user.js
@@ -0,0 +1,46 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const user_1 = require("../services/user");
+const router = express_1.default.Router();
+router.post("/register", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { firstName, lastName, email, password } = req.body;
+        const { statusCode, data } = yield (0, user_1.register)({
+            firstName,
+            lastName,
+            email,
+            password,
+        });
+        res.status(statusCode).json(data);
+    }
+    catch (error) {
+        console.error("Somthing went wrong !", error);
+    }
+}));
+router.post("/login", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { email, password } = req.body;
+        const { statusCode, data } = yield (0, user_1.login)({
+            email,
+            password,
+        });
+        res.status(statusCode).json(data);
+    }
+    catch (error) {
+        console.error("Somthing went wrong !", error);
+    }
+}));
+exports.default = router;
